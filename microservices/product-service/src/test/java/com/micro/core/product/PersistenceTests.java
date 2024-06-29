94325cfb33187f2d0363225ac4a0e1149c40f1df
miloud <miloamg@gmail.com>
2024-06-29 01:22:58 +0100

    added databases persistence test for product

---
+++ microservices/product-service/src/test/java/com/micro/core/product/PersistenceTests.java
@@ -0,0 +1,9 @@
+package com.micro.core.product;
+
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+//starts a database automaticlly
+@DataMongoTest
+public class PersistenceTests extends MongoTestBase{
+  
+}

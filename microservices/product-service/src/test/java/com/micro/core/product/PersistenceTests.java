d37f1f938147178775bdfac272585e2549396bf5
miloud <miloamg@gmail.com>
2024-07-02 22:35:27 +0100

    added setup function

--- microservices/product-service/src/test/java/com/micro/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/com/micro/core/product/PersistenceTests.java
@@ -1,9 +1,21 @@
 package com.micro.core.product;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import com.micro.core.product.persistence.ProductEntity;
+import com.micro.core.product.persistence.ProductRepository;
 
 //starts a database automaticlly
 @DataMongoTest
 public class PersistenceTests extends MongoTestBase{
-  
+  @Autowired
+  private ProductRepository repository;
+  private ProductEntity savedEntity;
+
+  @BeforeEach
+  void setUp() {
+
+  }
 }

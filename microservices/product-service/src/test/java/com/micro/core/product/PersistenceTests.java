11df6c8e2d495ef38963d29ecac4615f860c55ce
miloud <miloamg@gmail.com>
2024-07-05 02:06:07 +0100

    added assert function

--- microservices/product-service/src/test/java/com/micro/core/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/com/micro/core/product/PersistenceTests.java
@@ -1,5 +1,7 @@
 package com.micro.core.product;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -9,13 +11,25 @@ import com.micro.core.product.persistence.ProductRepository;
 
 //starts a database automaticlly
 @DataMongoTest
-public class PersistenceTests extends MongoTestBase{
+public class PersistenceTests extends MongoTestBase {
   @Autowired
   private ProductRepository repository;
   private ProductEntity savedEntity;
 
   @BeforeEach
   void setUp() {
+    repository.deleteAll();
 
+    ProductEntity entity = new ProductEntity(1, "product 1", 45);
+    savedEntity = repository.save(entity);
+    assertEqualsProduct(entity, savedEntity);
+  }
+
+  private void assertEqualsProduct(ProductEntity assertedEntity, ProductEntity actualEntity) {
+    assertEquals(assertedEntity.getId(), actualEntity.getId());
+    assertEquals(assertedEntity.getVersion(), actualEntity.getVersion());
+    assertEquals(assertedEntity.getName(), actualEntity.getName());
+    assertEquals(assertedEntity.getProductId(), actualEntity.getProductId());
+    assertEquals(assertedEntity.getWeight(), actualEntity.getWeight());
   }
 }
